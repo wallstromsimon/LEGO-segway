c6f08a9260d6645faa3143613a2b411aaa00e320
Simon <wallstrom.simon@gmail.com>
2014-11-08 17:12:12 +0100

    hello world

--- java_segway/src/java_segway/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package java_segway;
-
-public class helloworld {
-	public static void main(String[] args){
-		System.out.println("Hello robot");
-		System.out.println("Hello human");
-	}
-}
---
+++ java_segway/src/java_segway/helloworld.java
@@ -0,0 +1,8 @@
+package java_segway;
+
+public class helloworld {
+	public static void main(String[] args){
+		System.out.println("Hello robot");
+		System.out.println("Hello human");
+	}
+}
