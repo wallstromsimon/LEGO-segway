aa77bca3873a820e7e02a98e4cdcb53faebde90d
Simon <wallstrom.simon@gmail.com>
2014-11-18 16:30:23 +0100

    no more up.bat eftersom ALLT FUNKAR :dancers:

--- java_segway/src/up.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\simon\Documents\GitHub\LEGO-segway\java_segway\src
-
-call nxjc java_segway/*.java
-call nxjlink -o HelloWorld.nxj java_segway/helloworld
-call nxjupload -r HelloWorld.nxj
