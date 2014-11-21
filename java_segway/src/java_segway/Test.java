cafcdc8794a08fbbd0e4cfb77d6a47710d08fac1
Simon <wallstrom.simon@gmail.com>
2014-11-21 20:15:53 +0100

    Skrev ner lite tankar i testklassen :laughing:

--- java_segway/src/java_segway/Test.java
+++ java_segway/src/java_segway/Test.java
@@ -4,6 +4,7 @@ import lejos.nxt.Button;
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
+import lejos.nxt.addon.GyroDirectionFinder;
 import lejos.nxt.addon.GyroSensor;
 
 public class Test {
@@ -16,6 +17,15 @@ public class Test {
 			System.out.println(m.getAngle());
 			Button.waitForAnyPress();
 		}
+	}
+	
+	public void testThisNextTime(){
+		GyroSensor gyro = new GyroSensor(SensorPort.S2);
+		//use heading calc from run method in GyroDirectionFinder to calculate angle in io class! plsWork!
+		GyroDirectionFinder g = new GyroDirectionFinder(gyro);
+		//En annan tanke är att vi kanske ska sätta hastigheten på motorn istället för vart vi vill att den ska åka
+		//och sen bara säga om den ska åka frammåt eller bakåt.
+		
 	}
 
 }
